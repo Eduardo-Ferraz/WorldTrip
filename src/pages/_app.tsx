35c9745b8ccec36cc3ce9ffc0d2567f0feac3e99
Eduardo Ferraz <eduardoc.ferraz2@gmail.com>
2022-12-14 19:57:22 -0300

    feat: Fonte Poppins adicionada

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,15 @@
-import { ChakraProvider } from '@chakra-ui/react'
+import { ChakraProvider, extendTheme } from '@chakra-ui/react'
 import type { AppProps } from 'next/app'
+
+const theme = extendTheme({
+  fonts: {
+    body: "Poppins, sans-serif",
+  }
+})
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <Component {...pageProps} />
     </ChakraProvider>
   )
