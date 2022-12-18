abd48d8ab2eedbb58a602a58e158562bfcdfa4b7
EmillyNeves <emilly.neves@edu.ufes.br>
2022-12-17 23:44:32 -0300

    feat: Travel_types

--- components/Travel_types.tsx
+++ components/Travel_types.tsx
@@ -1,71 +1,87 @@
-import { Box, Flex, Input, Text } from '@chakra-ui/react'
+import { Box, Flex, Input, Show, Text } from '@chakra-ui/react'
 import { Stack, HStack, VStack } from '@chakra-ui/react'
 import { Image } from '@chakra-ui/react'
 import React from 'react';
+import { Icon, createIcon } from '@chakra-ui/react'
+import { Link } from '@chakra-ui/react'
+const breakpoints = {
+  sm: '30em',
+  md: '48em',
+  lg: '62em',
+  xl: '80em',
+  '2xl': '96em',
+}
+const CircleIcon = (props) => (
+  <Icon viewBox='0 0 200 200' {...props}>
+    <path
+      fill='currentColor'
+      d='M 100, 100 m -75, 0 a 75,75 0 1,0 150,0 a 75,75 0 1,0 -150,0'
+    />
+  </Icon>
+)
 
 export function Travel_types()
 {
-return (
-  <Stack 
-    direction={['column', 'row']}
-    spacing={['30px', '150px']}
-    pt={300}
-    fontSize='xl'
-    h="100vh"
-    justifyContent="center" 
-    alignItems="center"
-    bg="white" 
-  >
-    <Stack justifyContent="center" alignItems={"center"}>
-      <Image
-        boxSize='70px'
-        objectFit='fill'
-        src='/icon_vida_noturna.png'
-      />
-      <Text pt={4} as='b' color='dark.headtext'>
-        vida noturna
-      </Text>
+  return (
+    <Stack
+      direction={['column', 'row']}
+      spacing={['30px', '150px']}
+      pt={300}
+      fontSize='xl'
+      h="100vh"
+      justifyContent="center"
+      alignItems="center"
+      bg="white">
+      <Stack justifyContent="center" alignItems={"center"}>
+        <Image
+          boxSize='70px'
+          objectFit='fill'
+          src='/icon_vida_noturna.png'
+        />
+        <Text pt={4} as='b' color='dark.headtext'>
+          vida noturna
+        </Text>
+      </Stack>
+      <Stack justifyContent="center" alignItems={"center"}>
+        <Image
+          boxSize='70px'
+          objectFit='fill'
+          src='/icon_praia.png'
+        />
+        <Text pt={4} as='b' color='dark.headtext'>
+          praia
+        </Text>
+      </Stack>
+      <Stack justifyContent="center" alignItems={"center"}>
+        <Image
+          boxSize='70px'
+          objectFit='fill'
+          src='/icon_moderno.png'
+        />
+        <Text pt={4} as='b' color='dark.headtext'>
+          moderno
+        </Text>
+      </Stack>
+      <Stack justifyContent="center" alignItems={"center"}>
+        <Image
+          boxSize='70px'
+          objectFit='fill'
+          src='/icon_classico.png'
+        />
+        <Text pt={4} as='b' color='dark.headtext'>
+          clássico
+        </Text>
+      </Stack>
+      <Stack justifyContent="center" alignItems={"center"}>
+        <Image
+          boxSize='70px'
+          objectFit='fill'
+          src='/icon_mais.png'
+        />
+        <Text pt={4} as='b' color='dark.headtext'>
+          e mais...
+        </Text>
+      </Stack>
     </Stack>
-    <Stack justifyContent="center" alignItems={"center"}>
-      <Image
-        boxSize='70px'
-        objectFit='fill'
-        src='/icon_praia.png'
-      />
-      <Text pt={4} as='b' color='dark.headtext'>
-        praia
-      </Text>
-    </Stack>
-    <Stack justifyContent="center" alignItems={"center"}>
-      <Image
-        boxSize='70px'
-        objectFit='fill'
-        src='/icon_moderno.png'
-      />
-      <Text pt={4} as='b' color='dark.headtext'>
-        moderno
-      </Text>
-    </Stack>
-    <Stack justifyContent="center" alignItems={"center"}>
-      <Image
-        boxSize='70px'
-        objectFit='fill'
-        src='/icon_classico.png'
-      />
-      <Text pt={4} as='b' color='dark.headtext'>
-        clássico
-      </Text>
-    </Stack>
-    <Stack justifyContent="center" alignItems={"center"}>
-      <Image
-        boxSize='70px'
-        objectFit='fill'
-        src='/icon_mais.png'
-      />
-      <Text pt={4} as='b' color='dark.headtext'>
-        e mais...
-      </Text>
-    </Stack>
-  </Stack>
-);
+  );
 }
