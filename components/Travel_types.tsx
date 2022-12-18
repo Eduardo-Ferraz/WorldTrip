1208d0b24745e65fa378c1f6ad2d5606c6c8b360
EmillyNeves <emilly.neves@edu.ufes.br>
2022-12-18 00:06:54 -0300

    fix: Travel_types

--- components/Travel_types.tsx
+++ components/Travel_types.tsx
@@ -12,76 +12,114 @@ const breakpoints = {
   '2xl': '96em',
 }
 const CircleIcon = (props) => (
-  <Icon viewBox='0 0 200 200' {...props}>
-    <path
-      fill='currentColor'
-      d='M 100, 100 m -75, 0 a 75,75 0 1,0 150,0 a 75,75 0 1,0 -150,0'
-    />
-  </Icon>
+    <Icon viewBox='0 0 200 200' {...props}>
+        <path
+            fill='currentColor'
+            d='M 100, 100 m -75, 0 a 75,75 0 1,0 150,0 a 75,75 0 1,0 -150,0'
+        />
+    </Icon>
 )
 
 export function Travel_types()
 {
-  return (
-    <Stack
-      direction={['column', 'row']}
-      spacing={['30px', '150px']}
-      pt={300}
-      fontSize='xl'
-      h="100vh"
-      justifyContent="center"
-      alignItems="center"
-      bg="white">
-      <Stack justifyContent="center" alignItems={"center"}>
-        <Image
-          boxSize='70px'
-          objectFit='fill'
-          src='/icon_vida_noturna.png'
-        />
-        <Text pt={4} as='b' color='dark.headtext'>
-          vida noturna
-        </Text>
-      </Stack>
-      <Stack justifyContent="center" alignItems={"center"}>
-        <Image
-          boxSize='70px'
-          objectFit='fill'
-          src='/icon_praia.png'
-        />
-        <Text pt={4} as='b' color='dark.headtext'>
-          praia
-        </Text>
-      </Stack>
-      <Stack justifyContent="center" alignItems={"center"}>
-        <Image
-          boxSize='70px'
-          objectFit='fill'
-          src='/icon_moderno.png'
-        />
-        <Text pt={4} as='b' color='dark.headtext'>
-          moderno
-        </Text>
-      </Stack>
-      <Stack justifyContent="center" alignItems={"center"}>
-        <Image
-          boxSize='70px'
-          objectFit='fill'
-          src='/icon_classico.png'
-        />
-        <Text pt={4} as='b' color='dark.headtext'>
-          clássico
-        </Text>
-      </Stack>
-      <Stack justifyContent="center" alignItems={"center"}>
-        <Image
-          boxSize='70px'
-          objectFit='fill'
-          src='/icon_mais.png'
-        />
-        <Text pt={4} as='b' color='dark.headtext'>
-          e mais...
-        </Text>
-      </Stack>
-    </Stack>
-  );
+    return (
+        <Stack direction={['column', 'row']}
+        spacing={['30px', '150px']}
+        pt={300}
+        fontSize='xl'
+        h={['30vh', '100vh']}
+        justifyContent="center"
+        alignItems="center"
+        bg="white"
+        ><Show above='sm'>
+                <Link href='/' isExternal style={{ textDecoration: 'none' }}>
+                    <Stack justifyContent="center" alignItems={"center"} >
+                        <Image
+                            objectFit='fill'
+                            src='/icon_vida_noturna.png'
+                            w={[0, 0, 70]}
+                        />
+                        <Text pt={4} as='b' color='dark.headtext'>
+                            vida noturna
+                        </Text>
+                    </Stack>
+                </Link>
+                <Link href='/' isExternal style={{ textDecoration: 'none' }}>
+                    <Stack justifyContent="center" alignItems={"center"}>
+                        <Image
+                            boxSize={[0, 70, 70]}
+                            objectFit='fill'
+                            src='/icon_praia.png'
+                        />
+                        <Text pt={4} as='b' color='dark.headtext'>
+                            praia
+                        </Text>
+                    </Stack>
+                </Link>
+                <Link href='/' isExternal style={{ textDecoration: 'none' }}>
+                    <Stack justifyContent="center" alignItems={"center"}>
+                        <Image
+                            boxSize={[0, 70, 70]}
+                            objectFit='fill'
+                            src='/icon_moderno.png'
+                        />
+                        <Text pt={4} as='b' color='dark.headtext'>
+                            moderno
+                        </Text>
+                    </Stack>
+                </Link>
+                <Link href='/' isExternal style={{ textDecoration: 'none' }}>
+                    <Stack justifyContent="center" alignItems={"center"}>
+                        <Image
+                            boxSize={[0, 70, 70]}
+                            objectFit='fill'
+                            src='/icon_classico.png'
+                        />
+                        <Text pt={4} as='b' color='dark.headtext'>
+                            clássico
+                        </Text>
+                    </Stack>
+                </Link>
+                <Link href='/' isExternal style={{ textDecoration: 'none' }}>
+                    <Stack justifyContent="center" alignItems={"center"}>
+                        <Image
+                            boxSize={[0, 70, 70]}
+                            objectFit='fill'
+                            src='/icon_mais.png'
+                        />
+                        <Text pt={4} as='b' color='dark.headtext'>
+                            e mais...
+                        </Text>
+                    </Stack>
+                </Link>
+
+            </Show>
+
+
+            <Show breakpoint='(max-width: 400px)'>
+                <Link href='/' isExternal style={{ textDecoration: 'none' }}>
+                    <Stack fontSize={'18'} >
+                        <Box pt={4} color='dark.headtext'>
+                            <CircleIcon color='highlight' pr={2} />vida noturna
+                            <CircleIcon color='highlight' pr={2} ml={'16'} />praia
+                        </Box>
+                    </Stack>
+                </Link>
+                <Link href='/' isExternal style={{ textDecoration: 'none' }}>
+                    <Stack justifyContent="center" alignItems={"center"} fontSize={'18'}>
+                        <Text pt={4} color='dark.headtext' >
+                            <CircleIcon color='highlight' pr={2} />moderno
+                            <CircleIcon color='highlight' pr={2} ml={'16'} />clássico
+                        </Text>
+                    </Stack>
+                </Link>
+                <Link href='/' isExternal style={{ textDecoration: 'none' }}> <Stack justifyContent="center" alignItems={"center"} fontSize={'18'}>
+                    <Text pt={4} color='dark.headtext' >
+                        <CircleIcon color='highlight' pr={2} />  e mais...
+                    </Text>
+                </Stack>
+                </Link>
+            </Show>
+        </Stack>
+    );
 }
