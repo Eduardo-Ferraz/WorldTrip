df725dd91dc08c548ff3fd551c98e6ef5996d271
EmillyNeves <emilly.neves@edu.ufes.br>
2022-12-21 21:53:26 -0300

    feat: Página Ásia

--- src/pages/asia.tsx
+++ src/pages/asia.tsx
@@ -41,7 +41,7 @@ export default function America()
       console.log(photos);
     }
 
-    getNewPhoto(["rio_de_janeiro sightseeing", "machu sightseeing", "new_york sightseeing", "argentine sightseeing", "mexico sightseeing"]);
+    getNewPhoto(["hong kong sightseeing", "tokyo city", "korea", "istambul", "russia sightseeing"]);
 
   }, []);
 
@@ -65,17 +65,15 @@ export default function America()
             alignItems="center"
             spacing={'45px'}
           >
-            <Show above='sm'>
-              <Text mt={'10%'} pr={12} color='dark.headtext' w={'600px'} fontSize='24'>
+            <Show above='sm'  >
+              <Text mt={'10%'} pr={6} color='dark.headtext' w={'600px'} fontSize='24'>
                 A Ásia é o maior dos continentes, tanto em área como em população, faz fronteira no
                 lado ocidental com a África e com a Europa, com o oceano Pacífico, a Oceania e com a
-                América do Norte, pelo Estreito de Bering. Na Ásia são encontradas algumas das
-                montanhas mais altas do mundo; os rios mais extensos; os maiores desertos, planícies e
-                planaltos; as selvas e florestas mais densas
+                América do Norte, pelo Estreito de Bering. 
               </Text>
               <Stack justifyContent="center" alignItems={"center"} mt={10}>
                 <Text as='b' color='highlight' fontSize='48'>
-                  35
+                  49
                 </Text>
                 <Text as='b' color='dark.headtext'>
                   países
@@ -83,7 +81,7 @@ export default function America()
               </Stack>
               <Stack justifyContent="center" alignItems={"center"} >
                 <Text as='b' color='highlight' fontSize='48'>
-                  34
+                  199
                 </Text>
                 <Text as='b' color='dark.headtext'>
                   línguas
@@ -91,7 +89,7 @@ export default function America()
               </Stack>
               <Stack justifyContent="center" alignItems={"center"} >
                 <Text as='b' color='highlight' fontSize='48'>
-                  20
+                  14
                 </Text>
                 <Text as='b' color='dark.headtext'>
                   cidades +100
@@ -107,14 +105,14 @@ export default function America()
 
             <Show below='sm'>
               <Text justifyContent="space-between" color='dark.headtext' w={'300px'} fontSize='14' pb={30}>
-                A América é o segundo maior continente em área, localizado no hemisfério ocidental e que se
-                estende, no sentido norte-sul, desde o oceano Ártico até o cabo Horn, ao longo de cerca de
-                15 mil quilômetros
+                A Ásia é o maior dos continentes, tanto em área como em população, faz fronteira no
+                lado ocidental com a África e com a Europa, com o oceano Pacífico, a Oceania e com a
+                América do Norte, pelo Estreito de Bering.
               </Text>
               <Stack direction={'row'} spacing={'40px'}>
                 <Stack justifyContent="center" alignItems={"center"} >
                   <Text as='b' color='highlight' fontSize='32'>
-                    35
+                    49
                   </Text>
                   <Text color='dark.headtext'>
                     países
@@ -122,7 +120,7 @@ export default function America()
                 </Stack>
                 <Stack justifyContent="center" alignItems={"center"} >
                   <Text as='b' color='highlight' fontSize='32'>
-                    34
+                    199
                   </Text>
                   <Text color='dark.headtext'>
                     línguas
@@ -130,7 +128,7 @@ export default function America()
                 </Stack>
                 <Stack justifyContent="center" alignItems={"center"} >
                   <Text as='b' color='highlight' fontSize='32'>
-                    20
+                    14
                   </Text>
                   <Text color='dark.headtext' >
                     cidades +100
@@ -142,40 +140,40 @@ export default function America()
           <Image m={1} src='/icon_Info.png' w={[3, 4]} pt={[240, 20]} />
         </Flex>
 
-        <Text color='dark.headtext' fontSize='36' ml='10%' mt='5%' mb='5%'>Cidades +100</Text>
+        <Text  color='dark.headtext' fontSize='36' ml='10%' mt='5%' mb='5%'>Cidades +100</Text >
 
         {imagesUrl !== null && imagesUrl !== undefined && (
           <>
             <Grid mb={20} templateColumns={['repeat(1, 1fr)', 'repeat(2, 1fr)', 'repeat(3, 1fr)', 'repeat(4, 1fr)']} gap={6} ml='10%' mr='10%'>
               <Card_cidades
                 bg={imagesUrl[0].hits[Math.floor(Math.random() * imagesUrl[0].hits.length)].webformatURL}
-                title='Rio de Janeiro'
-                text='Brasil'
-                flag='https://cdn-icons-png.flaticon.com/512/551/551856.png'
+                title='Hong Kong'
+                text='China'
+                flag='https://cdn-icons-png.flaticon.com/512/197/197375.png'
               />
               <Card_cidades
                 bg={imagesUrl[1].hits[Math.floor(Math.random() * imagesUrl[1].hits.length)].webformatURL}
-                title='Machu Picchu'
-                text='Peru'
-                flag='https://cdn-icons-png.flaticon.com/512/197/197563.png'
+                title='Tokyo'
+                text='Japão'
+                flag='https://cdn-icons-png.flaticon.com/512/197/197604.png'
               />
               <Card_cidades
                 bg={imagesUrl[2].hits[Math.floor(Math.random() * imagesUrl[2].hits.length)].webformatURL}
-                title='Nova Iorque'
-                text='Estados Unidos'
-                flag='https://cdn-icons-png.flaticon.com/512/4628/4628635.png'
+                title='Seul'
+                text='Coreia do Sul'
+                flag='https://cdn-icons-png.flaticon.com/512/5111/5111586.png'
               />
               <Card_cidades
                 bg={imagesUrl[3].hits[Math.floor(Math.random() * imagesUrl[3].hits.length)].webformatURL}
-                title='Buenos Aires'
-                text='Argentina'
-                flag='https://cdn-icons-png.flaticon.com/512/197/197573.png'
+                title='Istambul'
+                text='Turquia'
+                flag='https://cdn-icons-png.flaticon.com/512/197/197518.png'
               />
               <Card_cidades
                 bg={imagesUrl[4].hits[Math.floor(Math.random() * imagesUrl[4].hits.length)].webformatURL}
-                title='Cidade do México'
-                text='México'
-                flag='https://cdn-icons-png.flaticon.com/512/5372/5372848.png'
+                title='Moscou'
+                text='Rússia'
+                flag='https://cdn-icons-png.flaticon.com/512/197/197408.png'
               />
             </Grid>
           </>
